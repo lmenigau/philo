ce7251e9779f8f5b78cb27ee059ddffa74e01a7f
Your Name <you@example.com>
2022-04-19 10:01:47 +0200

    rename philo to p

--- philo/philo.c
+++ philo/philo.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   p.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lomeniga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -33,101 +33,101 @@ void	long_write(t_mutex *lock, long *ptr, long val)
 	pthread_mutex_unlock(lock);
 }
 
-void	unlock_forks(t_philo *philo)
+void	unlock_forks(t_philo *p)
 {
-	if (philo->state == eating)
+	if (p->state == eating)
 	{
-		pthread_mutex_unlock(&philo->left->lock);
-		pthread_mutex_unlock(&philo->right->lock);
+		pthread_mutex_unlock(&p->left->lock);
+		pthread_mutex_unlock(&p->right->lock);
 	}
 }
 
-int	check_dead(t_philo *philo, long now)
+int	check_dead(t_philo *p, long now)
 {
-	pthread_mutex_lock(&philo->info->exit_l);
-	if (philo->info->exit)
+	pthread_mutex_lock(&p->info->exit_l);
+	if (p->info->exit)
 	{
-		pthread_mutex_unlock(&philo->info->exit_l);
-		unlock_forks(philo);
+		pthread_mutex_unlock(&p->info->exit_l);
+		unlock_forks(p);
 		return (0);
 	}
-	else if (now > philo->ts_dead)
+	else if (now > p->ts_dead)
 	{
-		philo->info->exit = 1;
-		pthread_mutex_unlock(&philo->info->exit_l);
-		ex_print("%5ld %3d died\n", philo->info->start, philo->id);
-		unlock_forks(philo);
+		p->info->exit = 1;
+		pthread_mutex_unlock(&p->info->exit_l);
+		ex_print("%5ld %3d died\n", p->info->start, p->id);
+		unlock_forks(p);
 		return (0);
 	}
-	pthread_mutex_unlock(&philo->info->exit_l);
+	pthread_mutex_unlock(&p->info->exit_l);
 	usleep(1);
 	return (1);
 }
 
-void	sleep_until(t_philo *philo, long dur)
+void	sleep_until(t_philo *p, long dur)
 {
 	long		now;
 
-	now = micro_ts(philo->info->start);
-	if (now + dur < philo->ts_dead)
+	now = micro_ts(p->info->start);
+	if (now + dur < p->ts_dead)
 		usleep(dur);
 	else
 	{
-		if (philo->ts_dead - now > 0)
-			usleep(philo->ts_dead - now);
-		philo->alive = 0;
+		if (p->ts_dead - now > 0)
+			usleep(p->ts_dead - now);
+		p->alive = 0;
 	}
 }
 
-void	eat(t_philo *philo)
+void	eat(t_philo *p)
 {
 	ex_print(
 		"%1$5ld %2$3d has taken a fork\n"
 		"%1$5ld %2$3d has taken a fork\n"
 		"%1$5ld %2$3d is eating\n",
-		philo->info->start, philo->id);
-	philo->last_meal = micro_ts(philo->info->start);
-	philo->ts_dead = philo->last_meal + philo->info->time_to_die;
-	sleep_until(philo, philo->info->eat_time);
-	pthread_mutex_unlock(&philo->left->lock);
+		p->info->start, p->id);
+	p->last_meal = micro_ts(p->info->start);
+	p->ts_dead = p->last_meal + p->info->time_to_die;
+	sleep_until(p, p->info->eat_time);
+	pthread_mutex_unlock(&p->left->lock);
 	usleep(1);
-	pthread_mutex_unlock(&philo->right->lock);
+	pthread_mutex_unlock(&p->right->lock);
 	usleep(1);
 }
 
-void	think(t_philo *philo)
+void	think(t_philo *p)
 {
 	long	ttl;
 
-	ex_print("%5ld %3d is thinking\n", philo->info->start, philo->id);
-	ttl = philo->ts_dead - micro_ts(philo->info->start);
-	sleep_until(philo, ttl / 2);
+	ex_print("%5ld %3d is thinking\n", p->info->start, p->id);
+	ttl = p->ts_dead - micro_ts(p->info->start);
+	sleep_until(p, ttl / 2);
 }
 
-void	philosopher(t_philo *philo)
+void	philosopher(t_philo *p)
 {
-	philo->ts_dead = micro_ts(philo->info->start) + philo->info->time_to_die;
-	if (philo->id & 1)
-		sleep_until(philo, 5000);
-	while (check_dead(philo, micro_ts(philo->info->start)))
+	p->ts_dead = micro_ts(p->info->start) + p->info->time_to_die;
+	if (p->id & 1)
+		sleep_until(p, 5000);
+	while (check_dead(p, micro_ts(p->info->start)))
 	{
-		if (philo->state == lock_Fork)
+		if (p->state == lock_Fork)
 		{
 			usleep(1);
-			pthread_mutex_lock(&philo->left->lock);
-			pthread_mutex_lock(&philo->right->lock);
+			pthread_mutex_lock(&p->left->lock);
+			pthread_mutex_lock(&p->right->lock);
 		}
-		else if (philo->state == eating)
-			eat(philo);
-		else if (philo->state == sleeping)
+		else if (p->state == eating)
+			eat(p);
+		else if (p->state == sleeping)
 		{
-			if (--philo->counter == 0)
+			if (--p->counter == 0)
 				break ;
-			ex_print("%5ld %3d is sleeping\n", philo->info->start, philo->id);
-			sleep_until(philo, philo->info->sleep_time);
+			ex_print("%5ld %3d is sleeping\n", p->info->start, p->id);
+			sleep_until(p, p->info->sleep_time);
 		}
-		else if (philo->state == thinking)
-			think(philo);
-		philo->state = (philo->state + 1) % (total_state);
+		else if (p->state == thinking)
+			think(p);
+		p->state = (p->state + 1) % (total_state);
 	}
 }
